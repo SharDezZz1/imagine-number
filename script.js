1a41cb2752e5fcff76f55d5016fc56a51fa56c26
SharDezZz1 <andriypidkova27@gmail.com>
2023-01-23 00:31:10 +0300

    total

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Загадайте число");
+alert("Помножте задумане число на 2")
+alert("Додайте до отриманого результату 7")
+
+const result = prompt("Введіть отриманий результат");
+
+let imaginedNum = (result - 7) / 2;
+
+alert("Ваше число " + imaginedNum)
